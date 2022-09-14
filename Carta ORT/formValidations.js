fb33c7f8e95459f4c49800058142a7995f636686
LucasRebasa <lucasrebasa02@hotmail.com>
2022-09-14 13:59:19 -0300

    Validacion select y mensaje error

--- Carta ORT/formValidations.js
+++ Carta ORT/formValidations.js
@@ -1,27 +1,50 @@
 function emailValidation() {
-    console.log("ingresa emailValidation()");
-    const input =  document.getElementById("email").value;
-    console.log("ingresa emailValidation() con el email: " + input);
-    var pattern = /^[A-Za-z0-9_!#$%&'*+\/=?`{|}~^.-]+@[A-Za-z0-9.-]+$/;
-    const isValidEmail = pattern.test(input);
+  console.log("ingresa emailValidation()");
+  const input =  document.getElementById("email");
+  console.log("ingresa emailValidation() con el email: " + input);
+  let pattern = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  const isValidEmail = pattern.test(input.value);
+  
+  console.log("validando...");
+  
+  if (isValidEmail) {
+    console.log("Valido")
+    if(input.nextElementSibling !== null){
+      input.nextElementSibling.remove()
+    }
+    return true;
+  } else {
+    console.log("Invalido")
+    const errorMsg = document.createElement('h4');
+    errorMsg.innerHTML = "Mail invalido";
+    errorMsg.classList.add('error')
+    if(!input.nextElementSibling.classList.contains('error')) input.after(errorMsg)
+    setFocusInElement("email");
+    return false;    
+  }    
+}
 
-    console.log("validando...");
+function selectValidation(){
+  const select = document.getElementById('referencia')
+  const errorMsg = document.createElement('h4');
 
-    if (isValidEmail) {
-        console.log("OK!");
-
-        alert("Valid email address!");
-        
-        return true;
-      } else {
-        console.log("email mal formado");
-
-        alert("Invalid email address!");
-        setFocusInElement("email");
-        return false;    
-      }    
+  if(select.selectedIndex === 0){
+    errorMsg.innerHTML = "Seleccione una opcion";
+    errorMsg.classList.add('error');
+    if(!select.nextElementSibling.classList.contains('error')) select.after(errorMsg)
+    return false;
+  }else if(select.nextElementSibling.classList.contains('error')){
+    select.nextElementSibling.remove()
+    return true;
+  }
 }
 
 function setFocusInElement (element) {
-    document.getElementById(element).focus();
+  document.getElementById(element).focus();
 }
+
+document.getElementById('submit').addEventListener('click', e =>{
+  let email = emailValidation();
+  let selectV = selectValidation()
+  if(email == false || selectV == false) e.preventDefault()
+})
